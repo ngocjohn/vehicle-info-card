2ba7d81cdfd8d5c06c2acf549333857b8dc19a4b
Viet Ngoc <ngocjohn@me.com>
2024-06-15 20:22:07 +0200

    rename map component

--- src/vehicle-info-card.ts
+++ src/vehicle-info-card.ts
@@ -25,7 +25,7 @@ import { tapFeedback } from './utils/tap-action.js';
 // Styles and Assets
 import styles from './css/styles.css';
 import { amgBlack, amgWhite } from './utils/imgconst';
-import './components/map-card-new';
+import './components/map-card';
 import './components/header-slide';
 import './components/eco-chart';
 
