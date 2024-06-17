3d0f66718a9c21926108f6253c3b1cc7c7d9f788
Viet Ngoc <ngocjohn@me.com>
2024-06-17 22:49:56 +0200

    update: splitting const and types

---
+++ src/const/data-keys.ts
@@ -0,0 +1,60 @@
+export const tripOverview = [
+  { key: 'odometer' },
+  { key: 'fuelLevel' },
+  { key: 'rangeLiquid', name: 'Range' },
+  { key: 'rangeElectric', name: 'Range' },
+  { key: 'soc' },
+  { key: 'maxSoc' },
+];
+
+export const tripFromReset = [
+  { key: 'distanceReset' },
+  { key: 'averageSpeedReset', icon: 'mdi:speedometer' },
+  { key: 'liquidConsumptionReset', name: 'Consumption reset' },
+  { key: 'electricConsumptionReset', name: 'Consumption reset' },
+];
+
+export const tripFromStart = [
+  { key: 'distanceStart' },
+  { key: 'averageSpeedStart', icon: 'mdi:speedometer-slow' },
+  { key: 'liquidConsumptionStart', name: 'Consumption start' },
+  { key: 'electricConsumptionStart', name: 'Consumption start' },
+];
+
+export const vehicleOverview = [{ key: 'lockSensor' }, { key: 'windowsClosed' }, { key: 'parkBrake' }];
+
+export const vehicleWarnings = [
+  { key: 'lowCoolantLevel' },
+  { key: 'lowBrakeFluid' },
+  { key: 'lowWashWater' },
+  { key: 'tirePressureWarning' },
+  { key: 'engineLight' },
+];
+
+export const ecoScores = [
+  { key: 'ecoScoreBonusRange', name: 'Bonus range' },
+  { key: 'ecoScoreAcceleraion', name: 'Acceleration' },
+  { key: 'ecoScoreConstant', name: 'Constant' },
+  { key: 'ecoScoreFreeWheel', name: 'Free wheel' },
+];
+
+export const tyrePressures = [
+  { key: 'tirePressureFrontLeft', name: 'Front left', icon: 'mdi:tire' },
+  { key: 'tirePressureFrontRight', name: 'Front right', icon: 'mdi:tire' },
+  { key: 'tirePressureRearLeft', name: 'Rear left', icon: 'mdi:tire' },
+  { key: 'tirePressureRearRight', name: 'Rear right', icon: 'mdi:tire' },
+];
+
+export const tyreAttributes = [
+  'tirePressureFrontRight',
+  'tirePressureFrontRight',
+  'tirePressureRearLeft',
+  'tirePressureRearRight',
+];
+
+export const chargingOverview = [
+  { key: 'chargingPower', name: 'Power', icon: 'mdi:flash' },
+  { key: 'soc', name: 'Current state' },
+  { key: 'maxSoc', name: 'Maximum' },
+  { key: 'selectedProgram' },
+];
