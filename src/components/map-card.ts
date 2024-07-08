3b6293ad6b902fa74be197b389b514007b357780
Viet Ngoc <ngocjohn@me.com>
2024-07-08 19:39:24 +0200

    update map background blending with main card

--- src/components/map-card.ts
+++ src/components/map-card.ts
@@ -22,74 +22,16 @@ interface Address {
 export class VehicleMap extends LitElement {
   @property({ attribute: false }) hass!: HomeAssistant;
   @property({ type: Object }) config!: VehicleCardConfig;
-
+  @property({ type: Boolean }) darkMode?: boolean;
   @state() private map: L.Map | null = null;
   @state() private marker: L.Marker | null = null;
   @state() private lat = 0;
   @state() private lon = 0;
-  @state() private zoom = 15;
+  @state() private zoom = 16;
   @state() private state = '';
   @state() private address: Partial<Address> = {};
   @state() private enableAdress = false;
   @state() private apiKey = '';
-
-  private get darkMode(): boolean {
-    if (this.config?.selected_theme?.mode === 'dark') {
-      return true;
-    } else if (this.config?.selected_theme?.mode === 'light') {
-      return false;
-    }
-    return this.hass.themes.darkMode;
-  }
-
-  firstUpdated(): void {
-    this.setEntityAttribute();
-  }
-
-  updated(changedProperties: PropertyValues): void {
-    if (changedProperties.has('darkMode')) {
-      this.updateCSSVariables();
-      this.updateMap();
-    }
-  }
-
-  setEntityAttribute(): void {
-    if (!this.config.device_tracker) return;
-    const deviceTracker = this.hass.states[this.config.device_tracker];
-    if (deviceTracker) {
-      this.lat = deviceTracker.attributes.latitude;
-      this.lon = deviceTracker.attributes.longitude;
-      this.state = deviceTracker.state;
-      this.getAddress(this.lat, this.lon);
-    }
-    setTimeout(() => {
-      this.initMap();
-    }, 200);
-  }
-
-  updateCSSVariables(): void {
-    if (this.darkMode) {
-      this.style.setProperty('--map-marker-color', 'var(--accent-color)');
-    } else {
-      this.style.setProperty('--map-marker-color', 'var(--primary-color)');
-    }
-  }
-
-  async getAddress(lat: number, lon: number): Promise<void> {
-    let address: Partial<Address> | null = null;
-    if (this.apiKey !== '') {
-      address = await this.getAddressFromGoggle(lat, lon);
-    } else {
-      address = await this.getAddressFromOpenStreet(lat, lon);
-    }
-    if (address) {
-      this.address = address;
-      this.enableAdress = true;
-      this.requestUpdate();
-    } else {
-      this.enableAdress = false;
-    }
-  }
 
   static get styles(): CSSResultGroup {
     return [
@@ -99,19 +41,39 @@ export class VehicleMap extends LitElement {
           outline: none;
         }
         :host {
-          --map-marker-color: var(--primary-color);
+          --vic-map-marker-color: var(--primary-color);
+          --vic-map-tiles-light-filter: none;
+          --vic-map-tiles-dark-filter: brightness(0.6) invert(1) contrast(3) hue-rotate(200deg) saturate(0.3)
+            brightness(0.7);
+          --vic-map-tiles-filter: var(--vic-map-tiles-light-filter);
+          --vic-map-mask-image: linear-gradient(to right, transparent 0%, black 10%, black 90%, transparent 100%),
+            linear-gradient(to bottom, transparent 10%, black 20%, black 90%, transparent 100%);
         }
         .map-wrapper {
+          position: relative;
           width: 100%;
           height: 100%;
+        }
+        .map-overlay {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background-color: var(--card-background-color);
+          opacity: 0.6; /* Adjust the opacity as needed */
+          pointer-events: none; /* Ensure the overlay does not interfere with map interactions */
         }
         #map {
           height: 100%;
           width: 100%;
           background: transparent !important;
-          mask-image: linear-gradient(to right, transparent 0%, black 10%, black 90%, transparent 100%),
-            linear-gradient(to bottom, transparent 10%, black 20%, black 90%, transparent 100%);
+          mask-image: var(--vic-map-mask-image);
           mask-composite: intersect;
+        }
+
+        .map-tiles {
+          filter: var(--vic-map-tiles-filter, none);
         }
 
         .marker {
@@ -127,28 +89,28 @@ export class VehicleMap extends LitElement {
           position: absolute;
           width: 14px;
           height: 14px;
-          background-color: var(--map-marker-color);
+          background-color: var(--vic-map-marker-color);
           border-radius: 50%;
           top: 50%;
           left: 50%;
           border: 1px solid white;
           transform: translate(-50%, -50%);
-          opacity: 0.6;
+          opacity: 1;
         }
         .shadow {
           position: absolute;
           width: 100%;
           height: 100%;
-          background-image: radial-gradient(circle, var(--map-marker-color) 0%, transparent 100%);
+          background-image: radial-gradient(circle, var(--vic-map-marker-color) 0%, transparent 100%);
           border-radius: 50%;
           top: 50%;
           left: 50%;
           transform: translate(-50%, -50%);
           border: none !important;
-          opacity: 0.3;
+          opacity: 0.6;
         }
         .marker:hover .dot {
-          opacity: 1;
+          transform: scale(1.2);
         }
         .leaflet-control-container {
           display: none;
@@ -176,7 +138,7 @@ export class VehicleMap extends LitElement {
           justify-content: center;
           flex-direction: column;
           gap: 0.5rem;
-          color: grey;
+          color: var(--primary-text-color);
           backdrop-filter: blur(1px);
           .address-line {
             display: flex;
@@ -192,6 +154,58 @@ export class VehicleMap extends LitElement {
     ];
   }
 
+  firstUpdated(): void {
+    this.updateCSSVariables();
+    this.setEntityAttribute();
+  }
+
+  updated(changedProperties: PropertyValues): void {
+    if (changedProperties.has('darkMode')) {
+      this.updateCSSVariables();
+      this.updateMap();
+    }
+  }
+
+  setEntityAttribute(): void {
+    if (!this.config.device_tracker) return;
+    const deviceTracker = this.hass.states[this.config.device_tracker];
+    if (deviceTracker) {
+      this.lat = deviceTracker.attributes.latitude;
+      this.lon = deviceTracker.attributes.longitude;
+      this.state = deviceTracker.state;
+      this.getAddress(this.lat, this.lon);
+    }
+    setTimeout(() => {
+      this.initMap();
+    }, 200);
+  }
+
+  private updateCSSVariables(): void {
+    if (this.darkMode) {
+      this.style.setProperty('--vic-map-marker-color', 'var(--accent-color)');
+      this.style.setProperty('--vic-map-tiles-filter', 'var(--vic-map-tiles-dark-filter)');
+    } else {
+      this.style.setProperty('--vic-map-marker-color', 'var(--primary-color)');
+      this.style.setProperty('--vic-map-tiles-filter', 'var(--vic-map-tiles-light-filter)');
+    }
+  }
+
+  async getAddress(lat: number, lon: number): Promise<void> {
+    let address: Partial<Address> | null = null;
+    if (this.apiKey !== '') {
+      address = await this.getAddressFromGoggle(lat, lon);
+    } else {
+      address = await this.getAddressFromOpenStreet(lat, lon);
+    }
+    if (address) {
+      this.address = address;
+      this.enableAdress = true;
+      this.requestUpdate();
+    } else {
+      this.enableAdress = false;
+    }
+  }
+
   initMap(): void {
     const mapOptions = {
       dragging: true,
@@ -204,8 +218,22 @@ export class VehicleMap extends LitElement {
       this.zoom,
     );
 
-    const tileLayer = this.darkMode ? 'CartoDB.DarkMatter' : 'CartoDB.Positron';
-    L.tileLayer.provider(tileLayer).addTo(this.map);
+    // const tileLayer = this.darkMode ? 'CartoDB.DarkMatter' : 'CartoDB.Positron'; Stadia.StamenTonerLite, CartoDB.PositronOnlyLabels
+    const provider = 'CartoDB.DarkMatterOnlyLabels';
+    const mapboxToken = process.env.MAPBOX_API;
+    const tileUrl = `https://api.mapbox.com/styles/v1/emkay2k9/clyd2zi0o00mu01pgfm6f6cie/tiles/{z}/{x}/{y}@2x?access_token=${mapboxToken}`;
+    // monochrome mapbox://styles/emkay2k9/clyd2cfiv00na01qpf32h6ybz
+
+    // navigation mapbox://styles/emkay2k9/clyd2zi0o00mu01pgfm6f6cie
+
+    L.tileLayer(tileUrl, {
+      maxZoom: 18,
+      tileSize: 512,
+      zoomOffset: -1,
+      className: 'map-tiles',
+    }).addTo(this.map);
+
+    // L.tileLayer.provider(provider).addTo(this.map);
 
     // Define custom icon for marker
     const customIcon = L.divIcon({
@@ -229,9 +257,10 @@ export class VehicleMap extends LitElement {
 
     this.marker.bindTooltip(this.state);
     this.updateMap();
+    this.updateCSSVariables();
   }
 
-  togglePopup(): void {
+  private togglePopup(): void {
     const event = new CustomEvent('toggle-map-popup', {
       detail: {},
       bubbles: true,
@@ -272,7 +301,8 @@ export class VehicleMap extends LitElement {
   render(): TemplateResult {
     return html`
       <div class="map-wrapper">
-        <div id="map" style=${this.darkMode ? 'filter: brightness(1.5);' : ''}></div>
+        <div id="map"></div>
+        <div class="map-overlay"></div>
         <div class="reset-button" @click=${this.updateMap}>
           <ha-icon icon="mdi:compass"></ha-icon>
         </div>
