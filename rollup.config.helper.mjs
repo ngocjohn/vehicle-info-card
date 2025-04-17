ac76fe06803a0b9a5b5cb91b19c95bdabe65589c
Viet Ngoc <ngocjohn@me.com>
2025-04-17 05:39:35 +0200

    chore: update dependencies and remove unused Babel plugin from Rollup configuration

--- rollup.config.helper.mjs
+++ rollup.config.helper.mjs
@@ -1,7 +1,6 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { babel } from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import postcssPresetEnv from 'postcss-preset-env';
 import postcssLit from 'rollup-plugin-postcss-lit';
@@ -38,10 +37,6 @@ export const defaultPlugins = [
   nodePolyfills(),
   commonjs(),
   typescript(),
-  babel({
-    babelHelpers: 'bundled',
-    exclude: 'node_modules/**',
-  }),
   json(),
   postcss({
     plugins: [
