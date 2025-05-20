ba81295a50d98893c4071d691582ded46c02078d
Viet Ngoc <96962827+ngocjohn@users.noreply.github.com>
2025-05-20 08:54:01 +0200

    Fix: Apply theme on element compabilty HA 2025.5.+ (#153)

--- src/utils/ha-helpers.ts
+++ src/utils/ha-helpers.ts
@@ -1,6 +1,5 @@
 const HELPERS = (window as any).loadCardHelpers ? (window as any).loadCardHelpers() : undefined;
 
-import { applyThemesOnElement } from 'custom-card-helpers';
 import { ExtraMapCardConfig, MapEntityConfig } from 'extra-map-card';
 import memoizeOne from 'memoize-one';
 
@@ -626,34 +625,6 @@ async function fetchLatestReleaseTag() {
     console.error('Error fetching the latest release tag:', error);
   }
 }
-
-export const applyTheme = (element: any, hass: HomeAssistant, theme: string, mode?: string): void => {
-  if (!element) return;
-  // console.log('applyTheme', theme, mode);
-  const themeData = hass.themes.themes[theme];
-  if (themeData) {
-    // Filter out only top-level properties for CSS variables and the modes property
-    const filteredThemeData = Object.keys(themeData)
-      .filter((key) => key !== 'modes')
-      .reduce((obj, key) => {
-        obj[key] = themeData[key];
-        return obj;
-      }, {} as Record<string, string>);
-
-    if (!mode || mode === 'auto') {
-      mode = hass.themes.darkMode ? 'dark' : 'light';
-      // Get the current mode (light or dark)
-    } else {
-      mode = mode;
-    }
-    const modeData = themeData.modes && typeof themeData.modes === 'object' ? themeData.modes[mode] : {};
-    // Merge the top-level and mode-specific variables
-    // const allThemeData = { ...filteredThemeData, ...modeData };
-    const allThemeData = { ...filteredThemeData, ...modeData };
-    const allTheme = { default_theme: hass.themes.default_theme, themes: { [theme]: allThemeData } };
-    applyThemesOnElement(element, allTheme, theme, false);
-  }
-};
 
 export const _convertToExtraMapConfig = (
   config: MapPopupConfig,
